441f9a71aa7baaf34ef94e47b3c7866984e95261
Rafael Di Genova <rafael.digenova@gmail.com>
2018-06-03 21:49:25 -0300

    Task -> Dashboard

--- FiapControleFinanceiro.UWP/Pages/DashboardPage.xaml.cs
+++ FiapControleFinanceiro.UWP/Pages/DashboardPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using FiapControleFinanceiro.UWP.Services;
+using FiapControleFinanceiro.UWP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 // O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -9,9 +12,27 @@ namespace FiapControleFinanceiro.UWP.Pages
     /// </summary>
     public sealed partial class DashboardPage : Page
     {
+        public DashBoardViewModel ViewModel { get; } = new DashBoardViewModel();
+
         public DashboardPage()
         {
             this.InitializeComponent();
+            this.Loaded += DashboardPage_Loaded;
+        }
+
+        private async void DashboardPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.Initialize();
+        }
+
+        public void AccountsVerMais_Clicked(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate<AccountsPage>();
+        }
+
+        public void TransactionsVerMais_Clicked(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate<TransactionsPage>();
         }
     }
 }
---
+++ FiapControleFinanceiro.UWP/ViewModels/DashBoardViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FiapControleFinanceiro.Models;
+using FiapControleFinanceiro.Models.Abstracts;
+using FiapControleFinanceiro.UWP.Pages;
+using FiapControleFinanceiro.UWP.Repository;
+using FiapControleFinanceiro.UWP.Services;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace FiapControleFinanceiro.UWP.ViewModels
+{
+    public class DashBoardViewModel : NotifyableClass
+    {
+        private EFAccountRepository AccountRepository { get; set; } = EFAccountRepository.Instance;
+        private EFTransactionsRepository TransactionRepository { get; set; } = EFTransactionsRepository.Instance;
+
+        public ObservableCollection<Account> Accounts => AccountRepository.Items;
+        public ObservableCollection<Transaction> Transactions => TransactionRepository.Items;
+
+        public async Task Initialize()
+        {
+            await AccountRepository.CarregarTodosAsync();
+            await TransactionRepository.CarregarTodosAsync();
+        }
+    }
+}
